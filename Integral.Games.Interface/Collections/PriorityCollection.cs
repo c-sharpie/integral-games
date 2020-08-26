982f9f07f818f5f24db63fcdd8059d9b919a2efe
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-08-26 10:41:14 -0400

    add weighted collection

---
+++ Integral.Games.Interface/Collections/PriorityCollection.cs
@@ -0,0 +1,11 @@
+﻿using Integral.Abstractions;
+using Integral.Aggregates;
+
+namespace Integral.Collections
+{
+    public interface PriorityCollection<Key, in Element> : PairedIncreasingAggregate<Key, Element>, IndirectDecreasingAggregate<Key>, Countable
+        where Key : notnull
+        where Element : notnull
+    {
+    }
+}
