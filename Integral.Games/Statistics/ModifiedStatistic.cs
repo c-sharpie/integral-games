b3f87fa44ac86173c7b08f300249441b351b94ce
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-09-25 12:49:58 -0400

    add modified statistic

---
+++ Integral.Games/Statistics/ModifiedStatistic.cs
@@ -0,0 +1,15 @@
+﻿namespace Integral.Statistics
+{
+    public class ModifiedStatistic : MultipliedStatistic
+    {
+        public ModifiedStatistic()
+        {
+            Register(Values);
+            Register(Modifiers);
+        }
+
+        public AggregateStatistic Values { get; } = new SummedStatistic();
+
+        public AggregateStatistic Modifiers { get; } = new SummedStatistic();
+    }
+}
